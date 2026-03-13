104bf3a121652560eef536e8730bf20591a5ad58
sumudu123466 <sumuduhasinika431@gmail.com>
2026-03-13 11:20:20 -0700

    a
    Signed-off-by: sumudu123466 <sumuduhasinika431@gmail.com>

--- src/main/java/lk/ijse/foodcity/dao/PaymentDAO.java
+++ src/main/java/lk/ijse/foodcity/dao/PaymentDAO.java
@@ -1,4 +1,10 @@
 package lk.ijse.foodcity.dao;
 
+import lk.ijse.foodcity.entity.Payment;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface PaymentDAO {
+    boolean save(Payment payment) throws SQLException;
+    List<Payment> getAll() throws SQLException;
 }
